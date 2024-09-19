a1158001ea0b2ef38f4238cff7922a3062c883fc
jose jimenez <jimenjos000@warren.k12.in.us>
2024-09-19 07:41:31 -0400

    hk

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    /**
+     * Returns true if number if a palindrome.
+     *
+     * @param number the number to check
+     * @return true if a palindrome.  False if not
+     */
+    public static boolean isPalindrome(int number){
+        number = Math.abs(number);
+
+        int numberClone = number;
+        int reverse = 0;
+
+        while(number > 0) {
+            // int last = number % 10;
+            // reverse = reverse * 10 + last;
+            // number = numberClone/10;
+
+            int lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            number /= 10;
+
+        }
+
+     
+
+
+
+        /*
+
+        Find the last digit of number with %.  number % 10 = right most digit
+        move reverse to the left.  Multiply by ten.  reverse *= 10;
+        Add last digit to reverse;
+
+        Repeat while number > 0
+            lastDigit = number % 10
+            reverse *= 10
+            reverse += lastDigit
+            number /= 10
+         */
+
+        return reverse == numberClone;
+
+
+
+    }
+
 }
